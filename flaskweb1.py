d91955d788009d1580779cc8e655e1e22829e8cd
Harsh Koli <harshkoli201@gmail.com>
2021-03-25 00:48:52 +0530

    Template

--- flaskweb1.py
+++ flaskweb1.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/home")
 def home():
-    return "Hello This is my First web page <h1> Flask Web Page </h1>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
